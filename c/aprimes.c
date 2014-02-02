12ed3647f767afc1d40bfc26b50b86662a46acdd
Anubhav Saini <IAmAnubhavSaini@GMail.com>
2014-02-02 19:34:29 +0530

    aprimes is_prime bug removed

--- c/aprimes.c
+++ c/aprimes.c
@@ -8,6 +8,10 @@ int is_prime(int num)
     return 0;
   }
 
+  if(num==2 || num==3 || num == 5 || num==7){
+    return 1;
+  }
+
   if(num%2==0 || num%3==0 || num%5==0 || num%7==0){
     return 0;
   }
